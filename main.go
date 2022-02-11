7dbeb8bdbf9f3838bc02e839056f25ae90609dd3
Maaz Shafi <maaz_shafi@hotmail.com>
2022-02-11 10:24:06 -0500

    capture user inputs

--- main.go
+++ main.go
@@ -3,23 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName string = "Go Conference"
+	conferenceName := "Go Conference"
 	const conferenceTickets int = 50
 	var remainingTicktets uint = 50
-
-	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTicktets, conferenceName)
 
 	fmt.Printf("Welcome to %v booking application!\n", conferenceName)
 	fmt.Printf("We have a total of %v tickets and %v are still available\n", conferenceTickets, remainingTicktets)
 	fmt.Println("Get your tickets here!")
 
-	var userName string
+	var firstName string
+	var lastName string
+	var email string
 	var userTickets int
 	// ask user for name
+	// pass the reference of the memory object
+	fmt.Println("Enter your fist name: ")
+	fmt.Scan(&firstName)
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+	fmt.Println("Enter your email: ")
+	fmt.Scan(&email)
+	fmt.Println("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
 
-	userName = "Tom"
-	userTickets = 2
-	fmt.Println(userName)
-	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will recieve a confirmation email at %v\n", firstName, lastName, userTickets, email)
 
 }
