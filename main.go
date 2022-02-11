b998013b7730073c5cead44cf01ffb9be273ba38
Maaz Shafi <maaz_shafi@hotmail.com>
2022-02-11 10:09:57 -0500

    learning types and interpolation

--- main.go
+++ main.go
@@ -3,5 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	var conferenceName string = "Go Conference"
+	const conferenceTickets int = 50
+	var remainingTicktets uint = 50
+
+	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTicktets, conferenceName)
+
+	fmt.Printf("Welcome to %v booking application!\n", conferenceName)
+	fmt.Printf("We have a total of %v tickets and %v are still available\n", conferenceTickets, remainingTicktets)
+	fmt.Println("Get your tickets here!")
+
+	var userName string
+	var userTickets int
+	// ask user for name
+
+	userName = "Tom"
+	userTickets = 2
+	fmt.Println(userName)
+	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+
 }
